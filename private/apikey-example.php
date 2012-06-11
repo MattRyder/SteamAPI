e53b9ffbe68d1a8d2005f720efab34e7231092ff
Matt Ryder <matt@mattryder.co.uk>
2012-06-11 17:52:58 +0100

    Added example APIKEY file

---
+++ private/apikey-example.php
@@ -0,0 +1,11 @@
+<?php
+
+/* rename to apikey.php
+ * put your API key (from https://steamcommunity.com/dev) into $apikey
+ * include into your application?
+ * NOTE: There's probably a safer way to do this than includes, I'll do some research...
+ */
+
+$apikey = "";
+?>
+
